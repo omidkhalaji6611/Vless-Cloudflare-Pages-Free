4438b5894b4b8c45d7660cf435fd187f2a5b32d4
omidkhalaji6611 <142794288+omidkhalaji6611@users.noreply.github.com>
2024-01-21 09:41:08 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 // @armanibash
 import { connect } from 'cloudflare:sockets';
 
-let userID = 'b6e8e835-bfdd-45ba-b4fc-c791866eb492';
+let userID = 'd0956eab-4da6-40eb-b066-c1f256eb2918';
 
 const proxyIPs = ["[2001:67c:2b0:db32:0:1:681a:404]"]; // ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
